f2f634c3aff2016f63e4ee7eea62a486447d6438
boyhcblackowl-coder <boy.hcblackowl@gmail.com>
2026-09-25 09:38:54 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.INSPIRE_CONFIG = {
+  // Paste your Cloudflare Worker URL here AFTER deploying the Worker.
+  // Example: https://inspire-api.your-subdomain.workers.dev
+  API_URL: 'https://inspire-api.boy-hcblackowl.workers.dev'
+};
